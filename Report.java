b0fa86c01aea528729ef5bd8af8292ab9d5ef4e4
JerryTheTus <tranvuongminhtu@gmail.com>
2025-01-29 13:41:39 +0800

    Update Report.java

--- Report.java
+++ Report.java
@@ -1,28 +1,29 @@
 import java.util.Scanner;
 
 public class Report {
-    public void GenerateReport (Scanner input) {
-    while (true) {
-      
-      int cake = input.nextInt();
-      switch (cake) {
-        case 1:
+  public void GenerateReport (Scanner input) {
+  while (true) {    
+    System.out.println("\n***Report Menu***\n1. Generate Orders report by Date \n2. Generate Orders report by Cake\n3. Generate Orders report by Customer\n4. Generate total number of Orders \n5. Back to the Main menu\n");
+    System.out.print("Enter option: ");
+    int cake = input.nextInt();
+    switch (cake) {
+      case 1:
         System.out.println();
         break;
-        case 2:
+      case 2:
         System.out.print("Enter Cake code: ");
         int cakeCode = input.nextInt();
         break;
         case 3:
         System.out.println();
         break;
-        case 4:
+      case 4:
         System.out.printf("Total number of Orders: ");
         break;
-        case 5:
+      case 5:
         System.out.println();
         return;
-        default:
+      default:
         System.out.println("Invalid option");
         return;
       }

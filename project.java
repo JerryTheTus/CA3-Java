1878c6427cd7eb27fed68ca8ef2ccaf59cf85013
JerryTheTus <tranvuongminhtu@gmail.com>
2025-01-29 13:41:10 +0800

    Update project.java

--- project.java
+++ project.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class project{
   public static void main (String[] args){
-    Cake menu1 = new Cake();
-    
-    Order menu3 = new Order();
-    Report menu4 = new Report();
+    Customer b = new Customer(0, null, "a");
+    Cake a = new Cake(0, null, 0);
+    Order c = new Order();
+    Report d = new Report();
     Scanner input = new Scanner(System.in); 
     while (true) {
       System.out.print("***Main Menu***\n1. Manage Cakes\n2. Manage Customers\n3. Manage Orders\n4. Generate Report\n0. Exit\n"); 
@@ -16,24 +16,21 @@ public class project{
         System.out.println("You have exited");
         return;
       case 1:
-        System.out.println("\n***Cake Menu***\n1. Create Cake\n2. Update Cake\n3. Delete Cake\n4. View all Cakes\n5. Back to the Main Menu\n");
-        System.out.print("Enter option: ");
+        a.CakeMenu(input);
         
-        menu1.CakeMenu(input);
         break;
       case 2:
-        System.out.println("\n***Customer Menu***\n1. Add Customer\n2. Edit Customer\n3. Remove Customer\n4. View all Customers\n5. Back to the Main menu\n");
-        System.out.print("Enter option: ");
+        
+        b.CustomerMenu(input);
+        
         break;
       case 3:
-        System.out.println("\n***Order Menu***\n1. Make an Order\n2. Cancel an Order\n3. View all Orders\n4. Back to the Main menu\n");
-        System.out.print("Enter option: ");
-        menu3.OrderCustomer(input);  
+        c.OrderCustomer(input);
+         
         break;
       case 4:
-        System.out.println("\n***Report Menu***\n1. Generate Orders report by Date \n2. Generate Orders report by Cake\n3. Generate Orders report by Customer\n4. Generate total number of Orders \n5. Back to the Main menu\n");
-        System.out.print("Enter option: ");
-        menu4.GenerateReport(input);
+        d.GenerateReport(input);
+        
         break;
       default:
         System.out.println("Invalid input");
